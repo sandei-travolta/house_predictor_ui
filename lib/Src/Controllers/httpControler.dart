0fc6b58695304b936bc7e3faf3358faf6e9e2954
Travolta Sandei <67733769+sandei-travolta@users.noreply.github.com>
2024-07-29 15:08:06 +0300

    predict prices

---
+++ lib/Src/Controllers/httpControler.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:house_predictor/Src/Models/PredictionModel.dart';
+import 'package:house_predictor/constants.dart';
+import 'package:http/http.dart' as http;
+import '../Models/Reponseodel.dart';
+
+Future<PredictionResponseModel?> makePrediction(int sizeSqft,int parkingLot,int rooms,int yearBuilt)async{
+  final url=Uri.parse("${apiUrl}/predict");
+  final PredictionRequestModel prediction=PredictionRequestModel(
+      sizeSqft: sizeSqft,
+      parkingLot: parkingLot,
+      rooms: rooms,
+      yearBuilt: yearBuilt
+  );
+  final body=jsonEncode(prediction.toJson());
+  final headers={'Content-Type':'application/json'};
+  final reponse=await http.post(url,body: body,headers: headers);
+  if(reponse.statusCode==200){
+    final Map<String,dynamic> responseBody=jsonDecode(reponse.body);
+    final PredictionResponseModel responseModel=PredictionResponseModel.fromJson(responseBody);
+    return responseModel;
+  }
+  return null;
+}
--- lib/Src/Screens/HousesPage/index.dart
+++ lib/Src/Screens/HousesPage/index.dart
@@ -6,12 +6,14 @@ class HouseListingpage extends StatelessWidget {
   TextEditingController parkingLotController=TextEditingController();
   TextEditingController roomsController=TextEditingController();
   TextEditingController yearBuitController=TextEditingController();
+  int price=0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           NumberTextFormField(
               emailController: sizeController,
@@ -33,6 +35,37 @@ class HouseListingpage extends StatelessWidget {
               labelText: "Year",
               hintText: "Year Built"
           ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Container(
+              height: 50,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.black87
+              ),
+              child: Center(
+                child: Text("Predict Price",style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w800
+                ),),
+              ),
+            ),
+          ),
+          Text(
+            "Price Approximation ${price}",
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.w600
+          ),
+          ),
+      Text(
+        "Matching Houses",
+        style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.w600
+        )
+      ),
         ],
       ),
     );
