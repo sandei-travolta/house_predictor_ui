4d623f40ed5cebc05f4a60bcf3d8951c11778201
Travolta Sandei <67733769+sandei-travolta@users.noreply.github.com>
2024-07-29 13:17:56 +0300

    set up home page

--- lib/Src/Screens/HomePage/index.dart
+++ lib/Src/Screens/HomePage/index.dart
@@ -1,9 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:house_predictor/Src/Screens/HousesPage/index.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Container(
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.black87
+              ),
+              child: Center(
+                child: Text("List House",style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w800
+                ),),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: InkWell(
+              onTap: ()=>Navigator.push(context,MaterialPageRoute(builder: (_)=>HouseListingpage())),
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.black87
+                ),
+                child: Center(
+                  child: Text("Looking For a House",style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w800
+                  ),),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Container(
+              height: 50,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.black87
+              ),
+              child: Center(
+                child: Text("Messages",style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w800
+                ),),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/Src/Screens/HousesPage/index.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class HouseListingpage extends StatelessWidget {
+  const HouseListingpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+
+      ),
+    );
+  }
+}
